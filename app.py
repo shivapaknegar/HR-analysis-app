303079f4168d83807ed5f06c2b939ad172acb1c2
shivapaknegar <shiva@paknegar.se>
2025-01-09 14:07:42 +0100

    Update app.py
    test flask

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 # main flask app
+from flask import Flask, render_template
+from models import db, Employee
+from utils import load_data, generate_visualizations
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.before_first_request
+def setup():
+    db.create_all()
+    load_data()
+
+@app.route('/')
+def dashboard():
+    employees = Employee.query.all()
+    generate_visualizations()
+    return render_template('dashboard.html', employees=employees)
+
+if __name__ == '__main__':
+    app.run(debug=True)
